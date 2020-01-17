d73fb6be2c2d15ea44575fddeded219cbba61904
MeganDoChi <maicdo74@gmail.com>
2020-01-17 11:16:57 +0700

    FirstVersion

---
+++ IdeaProjects/Utils/src/CodingTasks/MyStack.java
@@ -0,0 +1,63 @@
+package CodingTasks;
+
+
+//Stack implementation with Linked List
+public class MyStack {
+    public static void main (String[] args) {
+        Object[] items = new Object[] {"xml", 1, 2, "java"};
+
+        //Create a stack
+        StackOperation stack = new StackOperation();
+
+
+        for (Object i : items ) {
+            stack.push(i);
+        }
+
+        stack.pop();
+
+
+        System.out.println(stack.toString());
+
+    }
+}
+
+class StackOperation {
+    Element head;
+    Element previousHead;
+
+
+    public StackOperation() {
+        head = null;
+    }
+
+    public void push(Object i) {
+        Element newObject = new Element(i);
+
+        if (head == null) {
+            head = newObject;
+        }
+        else {
+           previousHead = head;
+           head = newObject;
+           head.setPointer(previousHead);
+        }
+    }
+
+    public void pop() {
+        head = previousHead;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+
+        while (previousHead != null) {
+            output += previousHead.getValue().toString() + " ";
+            previousHead = previousHead.getPointer();
+        }
+
+        return output;
+    }
+}
+
